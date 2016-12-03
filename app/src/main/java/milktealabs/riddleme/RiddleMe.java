a7f647813d2e7e99f0580590494aa7a777fa6b52
helenkuang <helen.kuang1@gmail.com>
2016-12-03 14:34:32 -0800

    Testing testing

---
+++ app/src/main/java/milktealabs/riddleme/RiddleMe.java
@@ -0,0 +1,15 @@
+package milktealabs.riddleme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RiddleMe extends AppCompatActivity {
+
+    //The thing that was here before
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_riddle_me);
+    }
+    
+}
