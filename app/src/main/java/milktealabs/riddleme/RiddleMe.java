0b637a208ed8246b03d2747021dcb33e9b2bb167
helenkuang <helen.kuang1@gmail.com>
2016-12-03 18:00:37 -0800

    Added comments and a bunch of helper functions in RiddleMe

--- app/src/main/java/milktealabs/riddleme/RiddleMe.java
+++ app/src/main/java/milktealabs/riddleme/RiddleMe.java
@@ -10,12 +10,22 @@ public class RiddleMe extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //TODO: move this to the RiddleList afterwards
+        //TODO: move this to the RiddleList afterwards when there is a main menu
+        //Pretty much all of this is supposed to be in the RiddleList portion
         setContentView(R.layout.riddlelist);
     }
 
     public void openRiddle(View view) {
-        
+        //jump table here for which riddle button pressed; open the riddle accordingly
         setContentView(R.layout.riddle1);
+        //after it is opened, call a method to actually have the functionality for a function?
+        while (!checkCorrect()){
+            //continue taking in user input
+        }
+    }
+    public boolean checkCorrect(){
+        //check whether or not the expected input is matched, else return false
+        //maybe take in which riddle we are talking about
+        return false;
     }
 }
